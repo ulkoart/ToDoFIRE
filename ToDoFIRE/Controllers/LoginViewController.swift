1088ee106037a433e7757bdf4946400f0450c37e
Artem Ulko <ulkoart@gmail.com>
2020-08-13 10:22:34 +0300

    UI init

---
+++ ToDoFIRE/Controllers/LoginViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  ToDoFIRE
+//
+//  Created by user on 11/08/2020.
+//  Copyright © 2020 Artem Ulko. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var warnLabel: UILabel!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func loginTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func registerTapped(_ sender: UIButton) {
+    }
+    
+    
+}
+
---
+++ ToDoFIRE/Controllers/TasksViewController.swift
@@ -0,0 +1,39 @@
+//
+//  TasksViewController.swift
+//  ToDoFIRE
+//
+//  Created by user on 13/08/2020.
+//  Copyright © 2020 Artem Ulko. All rights reserved.
+//
+
+import UIKit
+
+class TasksViewController: UIViewController {
+    
+    @IBOutlet private var tableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+
+    @IBAction func addTapped(_ sender: UIBarButtonItem) {
+    }
+}
+
+extension TasksViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.backgroundColor = .clear
+        cell.textLabel?.text = "This is cell number \(indexPath.row)"
+        cell.textLabel?.textColor = .white
+        return cell
+    }
+    
+    
+}
